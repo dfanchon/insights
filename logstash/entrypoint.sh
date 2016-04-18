fe1a8d018ce1499bbd838bdcf1cefb32d06209bf
Didier Fanchon <didier.fanchon@webtales.fr>
2016-04-18 11:23:28 +0200

    Refactoring

---
+++ logstash/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sed -i 's#mongoURI#${mongoURI}#g' /etc/logstash/conf.d/logstash.conf
+
+set -e
+
+# Add logstash as command if needed
+if [ "${1:0:1}" = '-' ]; then
+	set -- logstash "$@"
+fi
+
+# Run as user "logstash" if the command is "logstash"
+if [ "$1" = 'logstash' ]; then
+	set -- gosu logstash "$@"
+fi
+
+exec "$@"
