9988791ca60ea6794954e101e511fec05b6f139d
Didier Fanchon <didier.fanchon@webtales.fr>
2016-05-02 11:23:27 +0200

    Changes to mapping

--- elasticsearch/create_template.sh
+++ elasticsearch/create_template.sh
@@ -10,6 +10,25 @@ sleep 2
 curl -XPUT http://localhost:9200/_template/http -d '
 {
 	"template" : "http*",
+  "settings":{
+    "index":{
+      "analysis":{
+        "filter" : {
+          "url_filter": {
+            "type": "length",
+            "min" : 3
+          }
+        },
+        "analyzer":{
+          "analyzer_url":{
+            "type": "custom",
+            "tokenizer": "standard",
+            "filter":"url_filter"
+          }
+        }
+      }
+    }
+  },
 	"mappings" : {
    	"_default_" : {
   		"properties": {
@@ -18,15 +37,17 @@ curl -XPUT http://localhost:9200/_template/http -d '
   			"date": {"type": "date"},
   			"event": {"type": "string","index": "not_analyzed"},
   			"eventId": {"type": "string", "index":"not_analyzed"},
+				"eventLabel": {"type": "string", "index":"not_analyzed"},
   			"fingerprint": {"type": "string", "index": "not_analyzed"},
   			"lang": {"type": "string","index": "not_analyzed"},
   			"contenu": {"type": "string","index": "not_analyzed"},
   			"ctype": {"type": "string","index": "not_analyzed"},
   			"os": {"type": "string","index": "not_analyzed"},
+				"referrer": {"type": "string","index": "not_analyzed"},
   			"sessionId": {"type": "string", "index": "not_analyzed"},
   			"userId": {"type": "string","index": "not_analyzed"},
   			"url": {"type": "string", "index": "not_analyzed", "copy_to": "urlPath"},
-        "urlPath": {"type": "string"},
+        "urlPath": {"type": "string", "analyzer":"analyzer_url"},
         "geoip": {
           "dynamic": true,
           "type": "object",
